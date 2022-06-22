861d28c2b6a76c3aa7bea0bd4da62c2480ec4c0b
hyejin <hj10150@gmail.com>
2022-06-22 23:40:57 +0900

    first commit

--- main.js
+++ main.js
@@ -1,8 +1,6 @@
 const mobile__text = document.querySelector(".mobile__text");
 const last__list__errow = document.querySelector(".last__list__errow");
-const last_list = document.querySelector("last_list");
 
 mobile__text.addEventListener("click", () => {
-  last_list.style.animation = "make__rotate 0.5s infinite";
   last__list__errow.style.animation = "blink__arrow 1.2s forwards";
 });
