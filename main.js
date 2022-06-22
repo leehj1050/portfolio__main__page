5b3a53f523170c2eb406f7028f8580688cfacdbe
hyejin <hj10150@gmail.com>
2022-06-22 23:35:43 +0900

    first commit

--- main.js
+++ main.js
@@ -1,8 +1,8 @@
-const hover__div = document.querySelector(".hover__div");
+const mobile__text = document.querySelector(".mobile__text");
 const last__list__errow = document.querySelector(".last__list__errow");
 const last_list = document.querySelector("last_list");
 
-hover__div.addEventListener("click", () => {
+mobile__text.addEventListener("click", () => {
   last__list__errow.style.animation = "blink__arrow 1.5s forwards";
   last_list.style.animation = "make__rotate 0.5s ease infinite";
 });
